ad5f22bbca3c1d29cdb6dd13c683d30afa16dc04
evanchen <chenh_cnyn@163.com>
2021-11-24 15:15:19 +0800

    update

---
+++ layers/__init__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Time    :   2021/11/24 15:14:45
+@Author  :   Evan Chen 
+@Contact :   chenh_cnyn@163.com
+'''
+
+from adv_training import PGD, FGM
+
+__all__ = ['PGD', 'FGM']
